7c58e9b6d51abad7d899957702d423c01444e22b
Yuri <69178014+yuriwithdaggers@users.noreply.github.com>
2022-07-11 20:26:52 -0300

    Delete Herança para Diferença directory

--- Herança para Diferença/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Funcionario p1 = new Funcionario();
-    p1.setNome("John Doe");
-    p1.setTrabalhando(true);
-    p1.mudarTrabalho();
-    System.out.println(p1.getTrabalhando());
-  }
-}
